a2b01daddd47f97efd22aa3e0e5bef4a53bb6f1b
wartgowj <wartgowj@gmail.com>
2017-12-28 21:43:10 -0800

    location search functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 
 	var lat
 	var lng
-
+	//triggers ajaxByLocation on click
 	$(document).on("click", '#location-button', function(event){
 		event.preventDefault();
-		getLocation();
+		ajaxByLocation();
 	});
-
+	//gets the users location and stores in lat and lng
 	function getLocation() {
     	navigator.geolocation.getCurrentPosition(function(position) {
    			lat = position.coords.latitude;
@@ -15,10 +15,48 @@ $(document).ready(function(){
    			logResults();
     })
 }
-
+	//console logs user coordinates
 	function logResults(){
 		console.log("Latitude = " + lat);
 		console.log("Longitude = " + lng);
 	}
+	//initiates breweryDB ajax by user location
+	function ajaxByLocation(){
+		var key = "add62ea870e1bfdea1eee00e3594dbf5";
+		var radius = "1";
+		var queryLocationURL = "https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/search/geo/point?lat=" + lat + "&lng=" + lng + "&radius=" + radius + "&key=" + key +"";
 
+		$.ajax({
+			url: queryLocationURL,
+			method: "GET"
+		}).done(function(response) {
+			console.log(response.data);
+		//looping thru results and adding to table
+			var results = response.data;
+
+			for (var i = 0; i < results.length; i++){
+				var tdRow = $("<tr>");
+				var name = $("<td>");
+				name.text(results[i].brewery.name);
+				var dis = $("<td>")
+				dis.text(results[i].distance);
+				var address = $("<td>");
+				address.text(results[i].streetAddress);
+				var phone = $("<td>");
+				phone.text(results[i].phone);
+				tdRow.append(name);
+				tdRow.append(dis);
+				tdRow.append(address);
+				tdRow.append(phone);
+				$("#breweryList > tbody").append(tdRow); 
+			}
+
+
+
+		})
+	}
+getLocation();
 });
+
+
+//http://api.brewerydb.com/v2/search/geo/point?lat=32.9040647&lng=-117.13557039999999&radius=1&key=add62ea870e1bfdea1eee00e3594dbf5
