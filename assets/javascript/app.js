67e14073d2201676d24b0325cbf5f6fc0d172fe3
wartgowj <wartgowj@gmail.com>
2017-12-29 17:49:59 -0800

    zip search functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,10 +2,20 @@ $(document).ready(function(){
 
 	var lat
 	var lng
-	//triggers ajaxByLocation on click
+	var zip
+	//triggers ajaxByLocation
 	$(document).on("click", '#location-button', function(event){
 		event.preventDefault();
+		$("#breweryList > tbody").empty();
 		ajaxByLocation();
+	});
+	//triggers ajaxByZip
+	$(document).on("click", '#zipcode-button', function(event){
+		event.preventDefault();
+		zip = $("#zipcode").val().trim();
+		console.log(zip);
+		$("#breweryList > tbody").empty();
+		ajaxByZip();
 	});
 	//gets the users location and stores in lat and lng
 	function getLocation() {
@@ -24,7 +34,7 @@ $(document).ready(function(){
 	function ajaxByLocation(){
 		var key = "add62ea870e1bfdea1eee00e3594dbf5";
 		var radius = "1";
-		var queryLocationURL = "https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/search/geo/point?lat=" + lat + "&lng=" + lng + "&radius=" + radius + "&key=" + key +"";
+		var queryLocationURL = "https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/search/geo/point?lat=" + lat + "&lng=" + lng + "&radius=" + radius + "&key=" + key +"";
 
 		$.ajax({
 			url: queryLocationURL,
@@ -39,24 +49,58 @@ $(document).ready(function(){
 				var name = $("<td>");
 				name.text(results[i].brewery.name);
 				var dis = $("<td>")
-				dis.text(results[i].distance);
+				dis.text(results[i].distance + " miles");
 				var address = $("<td>");
 				address.text(results[i].streetAddress);
 				var phone = $("<td>");
 				phone.text(results[i].phone);
 				tdRow.append(name);
+				tdRow.append(address);
+				tdRow.append(phone);
 				tdRow.append(dis);
+				$("#breweryList > tbody").append(tdRow); 
+			}
+
+		})
+
+	}
+
+	//initiates breweryDB ajax by zipcode
+	function ajaxByZip(){
+		var key = "add62ea870e1bfdea1eee00e3594dbf5";
+		var queryZipURL = "https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/locations?key=" + key + "&postalCode=" + zip + "";
+
+		$.ajax({
+			url: queryZipURL,
+			method: "GET"
+		}).done(function(response) {
+			console.log(response.data);
+		//looping thru results and adding to table
+			var results = response.data;
+
+			for (var i = 0; i < results.length; i++){
+				var tdRow = $("<tr>");
+				var name = $("<td>");
+				name.text(results[i].brewery.name);
+				var address = $("<td>");
+				address.text(results[i].streetAddress);
+				var phone = $("<td>");
+				phone.text(results[i].phone);
+				tdRow.append(name);
 				tdRow.append(address);
 				tdRow.append(phone);
 				$("#breweryList > tbody").append(tdRow); 
 			}
-
-
-
 		})
-	}
+	}	
 getLocation();
+
 });
 
 
-//http://api.brewerydb.com/v2/search/geo/point?lat=32.9040647&lng=-117.13557039999999&radius=1&key=add62ea870e1bfdea1eee00e3594dbf5
+
+
+
+
+
+
